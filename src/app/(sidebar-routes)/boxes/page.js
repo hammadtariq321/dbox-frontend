ba54ce9eccbd4a2662f9b1f8191cd8f29c47f96c
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-08-05 20:37:19 +0500

    "Added react-data-table-component and styled-components dependencies, updated package-lock.json and package.json, and modified src/app/(sidebar-routes)/boxes/page.js to use DataTable component"

--- src/app/(sidebar-routes)/boxes/page.js
+++ src/app/(sidebar-routes)/boxes/page.js
@@ -1,8 +1,31 @@
+'use client'
+import { BoxColumns } from "@/columns/BoxColumns";
+import Heading from "@/components/Heading";
 import React from "react";
+import DataTable from "react-data-table-component";
+
+
+const data = [
+  {
+    id: 1,
+    title: 'Beetlejuice',
+    year: '1988',
+  },
+  {
+    id: 2,
+    title: 'Ghostbusters',
+    year: '1984',
+  },
+]
 
 function DonationBox() {
   return <>
-    <h1>Donation Box</h1>
+    <Heading />
+    <DataTable
+      columns={BoxColumns}
+      data={data}
+      className="mt-10"
+    />
   </>
 }
 
