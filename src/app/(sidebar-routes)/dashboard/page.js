46fdc7ce9cd66d0649db47394a3de478bce7d263
Hammad Tariq <47973106+hammadtariq321@users.noreply.github.com>
2024-08-08 22:48:15 +0500

    "Update DashboardPage: changed numerical values and added text-gray-700 class to three paragraphs"

--- src/app/(sidebar-routes)/dashboard/page.js
+++ src/app/(sidebar-routes)/dashboard/page.js
@@ -12,21 +12,21 @@ function DashboardPage() {
           <div className="bg-gray-100 h-[50px] w-[50px] flex items-center justify-center rounded-full">
             <LuCalendarDays />
           </div>
-          <p className="font-bold text-xl mt-5">Rs 25,000</p>
+          <p className="font-bold text-xl mt-5 text-gray-700">Rs 500</p>
           <p className="text-xs">Today</p>
         </div>
         <div className=" p-5 w-full rounded border hover:shadow cursor-pointer">
           <div className="bg-gray-100 h-[50px] w-[50px] flex items-center justify-center rounded-full">
             <FaRegCalendarCheck />
           </div>
-          <p className="font-bold text-xl mt-5">Rs 25,000</p>
+          <p className="font-bold text-xl mt-5 text-gray-700">Rs 25,000</p>
           <p className="text-xs">Current Month</p>
         </div>
         <div className=" p-5 w-full rounded border hover:shadow cursor-pointer">
           <div className="bg-gray-100 h-[50px] w-[50px] flex items-center justify-center rounded-full">
             <FaRegCalendarMinus />
           </div>
-          <p className="font-bold text-xl mt-5">Rs 25,000</p>
+          <p className="font-bold text-xl mt-5 text-gray-700">Rs 7,200</p>
           <p className="text-xs">Previous Month</p>
         </div>
       </div>
